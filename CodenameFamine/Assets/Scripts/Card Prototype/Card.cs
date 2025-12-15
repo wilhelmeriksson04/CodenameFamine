0ff09537407f8329bbc1c528a3a667a05f299337
Wilhelm Eriksson <wilhelm.eriksson04@gmail.com>
2025-12-15 23:59:35 +0100

    card prototype setup

---
+++ CodenameFamine/Assets/Scripts/Card Prototype/Card.cs
@@ -0,0 +1,147 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace CardPrototype
+{
+#if UNITY_EDITOR
+    using UnityEditor;
+    using CardPrototype;
+
+    [CustomEditor(typeof(Card))]
+    public class CardEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            //always visible
+            DrawProperty("cardName");
+            DrawProperty("cardType");
+
+            //get cardType list
+            SerializedProperty cardTypeProp = serializedObject.FindProperty("cardType");
+            bool isArtifact = HasType(cardTypeProp, Card.CardType.Artifact);
+            bool isCharacter = HasType(cardTypeProp, Card.CardType.Character);
+            bool isCreature = HasType(cardTypeProp, Card.CardType.Creature);
+            bool isProvision = HasType(cardTypeProp, Card.CardType.Provision);
+            bool isSpell = HasType(cardTypeProp, Card.CardType.Spell);
+            bool isWeapon = HasType(cardTypeProp, Card.CardType.Weapon);
+
+            //conditional fields
+            if (isArtifact)
+            {
+                EditorGUILayout.Space(); EditorGUILayout.LabelField("Artifact", EditorStyles.boldLabel);
+                DrawProperty("damageMax");
+                DrawProperty("damageMin");
+                DrawProperty("health");
+                DrawProperty("shieldAdd");
+                DrawProperty("damageType");
+            }
+
+            if (isCharacter)
+            {
+                EditorGUILayout.Space(); EditorGUILayout.LabelField("Character", EditorStyles.boldLabel);
+                DrawProperty("damageMax");
+                DrawProperty("damageMin");
+                DrawProperty("health");
+                DrawProperty("damageType");
+            }
+
+            if (isCreature)
+            {
+                EditorGUILayout.Space(); EditorGUILayout.LabelField("Creature", EditorStyles.boldLabel);
+                DrawProperty("ammoCount");
+                DrawProperty("damageMax");
+                DrawProperty("damageMin");
+                DrawProperty("damageType");
+            }
+
+            if (isProvision)
+            {
+                EditorGUILayout.Space(); EditorGUILayout.LabelField("Provision", EditorStyles.boldLabel);
+                DrawProperty("healthAdd");
+            }
+
+            if (isSpell)
+            {
+                EditorGUILayout.Space(); EditorGUILayout.LabelField("Spell", EditorStyles.boldLabel);
+                DrawProperty("damageMax");
+                DrawProperty("damageMin");
+                DrawProperty("healthAdd");
+                DrawProperty("manaCount");
+                DrawProperty("shieldAdd");
+                DrawProperty("shield");
+                DrawProperty("damageType");
+            }
+
+            if (isWeapon)
+            {
+                EditorGUILayout.Space(); EditorGUILayout.LabelField("Weapon", EditorStyles.boldLabel);
+                DrawProperty("ammoCount");
+                DrawProperty("damageMax");
+                DrawProperty("damageMin");
+                DrawProperty("range");
+                DrawProperty("damageType");
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void DrawProperty(string name)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty(name));
+        }
+
+        private bool HasType(SerializedProperty listProperty, params Card.CardType[] types)
+        {
+            for (int i = 0; i < listProperty.arraySize; i++)
+            {
+                var element = (Card.CardType)listProperty.GetArrayElementAtIndex(i).enumValueIndex;
+                foreach (var type in types)
+                {
+                    if (element == type)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+#endif
+
+    [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+    public class Card : ScriptableObject
+    {
+        public string cardName;
+        public List<CardType> cardType;
+        public List<DamageType> damageType;
+        public float health;
+        public float healthAdd;
+        public float damageMin;
+        public float damageMax;
+        public float range;
+        public int shieldAdd;
+        public int shield;
+        public int ammoCount;
+        public int manaCount;
+
+        public enum CardType
+        {
+            Artifact,
+            Character,
+            Creature,
+            Provision,
+            Spell,
+            Weapon
+        }
+
+        public enum DamageType
+        {
+            Bleed,
+            Magical,
+            Physical,
+            Poison,
+            Ranged
+        }
+    }
+}

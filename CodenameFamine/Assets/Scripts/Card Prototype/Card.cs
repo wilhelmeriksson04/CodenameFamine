e029fc0e2693456bb64bf77e963b217ab5f37246
Wilhelm Eriksson <wilhelm.eriksson04@gmail.com>
2025-12-16 12:33:59 +0100

    assets

--- CodenameFamine/Assets/Scripts/Card Prototype/Card.cs
+++ CodenameFamine/Assets/Scripts/Card Prototype/Card.cs
@@ -26,13 +26,13 @@ namespace CardPrototype
             bool isProvision = HasType(cardTypeProp, Card.CardType.Provision);
             bool isSpell = HasType(cardTypeProp, Card.CardType.Spell);
             bool isWeapon = HasType(cardTypeProp, Card.CardType.Weapon);
+            bool isAnyType = isArtifact || isCharacter || isCreature || isProvision || isSpell || isWeapon;
 
             //conditional fields
             if (isArtifact)
             {
                 EditorGUILayout.Space(); EditorGUILayout.LabelField("Artifact", EditorStyles.boldLabel);
-                DrawProperty("damageMax");
-                DrawProperty("damageMin");
+                DrawProperty("damage");
                 DrawProperty("health");
                 DrawProperty("shieldAdd");
                 DrawProperty("damageType");
@@ -41,8 +41,7 @@ namespace CardPrototype
             if (isCharacter)
             {
                 EditorGUILayout.Space(); EditorGUILayout.LabelField("Character", EditorStyles.boldLabel);
-                DrawProperty("damageMax");
-                DrawProperty("damageMin");
+                DrawProperty("damage");
                 DrawProperty("health");
                 DrawProperty("damageType");
             }
@@ -51,8 +50,7 @@ namespace CardPrototype
             {
                 EditorGUILayout.Space(); EditorGUILayout.LabelField("Creature", EditorStyles.boldLabel);
                 DrawProperty("ammoCount");
-                DrawProperty("damageMax");
-                DrawProperty("damageMin");
+                DrawProperty("damage");
                 DrawProperty("damageType");
             }
 
@@ -65,8 +63,7 @@ namespace CardPrototype
             if (isSpell)
             {
                 EditorGUILayout.Space(); EditorGUILayout.LabelField("Spell", EditorStyles.boldLabel);
-                DrawProperty("damageMax");
-                DrawProperty("damageMin");
+                DrawProperty("damage");
                 DrawProperty("healthAdd");
                 DrawProperty("manaCount");
                 DrawProperty("shieldAdd");
@@ -78,11 +75,15 @@ namespace CardPrototype
             {
                 EditorGUILayout.Space(); EditorGUILayout.LabelField("Weapon", EditorStyles.boldLabel);
                 DrawProperty("ammoCount");
-                DrawProperty("damageMax");
-                DrawProperty("damageMin");
+                DrawProperty("damage");
                 DrawProperty("range");
                 DrawProperty("damageType");
             }
+
+            if (isAnyType)
+                EditorGUILayout.HelpBox("You can assign multiple types to a card. The relevant fields will be shown above.", MessageType.Info);
+            else
+                EditorGUILayout.HelpBox("Please assign at least one Card Type to see relevant fields.", MessageType.Warning);
 
             serializedObject.ApplyModifiedProperties();
         }
@@ -97,6 +98,7 @@ namespace CardPrototype
             for (int i = 0; i < listProperty.arraySize; i++)
             {
                 var element = (Card.CardType)listProperty.GetArrayElementAtIndex(i).enumValueIndex;
+
                 foreach (var type in types)
                 {
                     if (element == type)
@@ -117,8 +119,7 @@ namespace CardPrototype
         public List<DamageType> damageType;
         public float health;
         public float healthAdd;
-        public float damageMin;
-        public float damageMax;
+        public float damage;
         public float range;
         public int shieldAdd;
         public int shield;
---
+++ CodenameFamine/Assets/Scripts/Card Prototype/CardDisplay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+using CardPrototype;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card cardData;
+    public Image cardImage;
+    public TMP_Text nameText;
+    public TMP_Text healthText;
+    public TMP_Text damageText;
+    public Image[] typeImage;
+
+    void Start()
+    {
+        UpdateCardDisplay();
+    }
+
+    public void UpdateCardDisplay()
+    {
+        nameText.text = cardData.cardName;
+        healthText.text = cardData.health.ToString();
+        damageText.text = cardData.damage.ToString();
+    }
+}
